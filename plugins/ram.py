d9589e029e137ebd83dce213dfefd0df4fc02a2a
sunnyQA233 <sunnyqa233@outlook.com>
2022-06-04 15:49:17 +0800

    Rewrite

--- plugins/ram.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-
-def get_ram_used_percentage() -> float:
-    return psutil.virtual_memory().percent
-
-
-def get_swap_used_percentage() -> float:
-    return psutil.swap_memory().percent
